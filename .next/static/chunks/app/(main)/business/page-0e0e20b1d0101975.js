bd1ef0aac940dbd1c2527542cd519175fb64819c
kenny-io <ekeneeze3@gmail.com>
2026-01-15 20:15:13 +0400

    fix: add .next to gitignore and remove from tracking

--- .next/static/chunks/app/(main)/business/page-0e0e20b1d0101975.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[49,778],{686:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[0,441,255,358],()=>e(e.s=686)),_N_E=e.O()}]);
